9feaa055c4cf36a00584eff1f8b437e97ef1c225
Glitch20057 <ma6962191@gmail.com>
2026-09-23 10:12:47 +0330

    feat: export Visualizer class in visitext package init

---
+++ visitext_init_.py
@@ -0,0 +1,21 @@
+from visitext.engine import VisiTextEngine
+from visitext.models import ProcessedDocument, TextBlock, BoundingBox
+from visitext.preprocessor import ImagePreprocessor
+from visitext.filters import TextFilter
+from visitext.layout import LayoutReconstructor, TextLine
+from visitext.extractors import DocumentDataExtractor, ExtractedDocumentData
+from visitext.visualizer import Visualizer
+
+__all__ = [
+    "VisiTextEngine",
+    "ProcessedDocument",
+    "TextBlock",
+    "BoundingBox",
+    "ImagePreprocessor",
+    "TextFilter",
+    "LayoutReconstructor",
+    "TextLine",
+    "DocumentDataExtractor",
+    "ExtractedDocumentData",
+    "Visualizer"
+]
